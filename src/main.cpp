addc95044fa51364991b3e4495b5667bd39ad082
Fauzi Abdul Rohim <fauziboyranger@gmail.com>
2024-07-23 12:00:52 +0700

    chang back

--- src/main.cpp
+++ src/main.cpp
@@ -20,13 +20,12 @@ DHT dht(DHTPIN, DHTTYPE);
 char auth[] = "mk0JrqsmyCLZ6ElAgxw4dL4-rkGLokDp"; // Ganti dengan token Blynk Anda
 char ssid[] = "YOUR_SSID"; // Ganti dengan SSID WiFi Anda
 char pass[] = "YOUR_PASSWORD"; // Ganti dengan password WiFi Anda
+
 // Deklarasi variabel input dan ouput fuzzy
-float suhu_z, kelembaban_z, durasi_z;
+float suhu_z, kelembaban_z, pwm_z;
 // Deklarasi fungsi pembulatan
 int roundsuhu;
 int roundkelembaban;
-// Deklarasi pin output ke LED
-int pindurasi = 4;
 
 /* Bagian 1: Definisi Type Bentukan Variabel Fuzzy */
 typedef struct suhu {
@@ -44,6 +43,7 @@ typedef struct kelembaban {
   float sangat_basah;
 };
 
+
 /* Bagian 2 : Definisi Fungsi Himpunan Fuzzy */
 float trap_mf (float a, float b, float c, float d, float x) {
   /* a,b,c, dan d adalah parameter trapesium */
@@ -57,6 +57,7 @@ float tri_mf(float a, float b, float c, float x) {
     Function-nya */
   return max(min(((x - a) / (b - a)), ( (c - x) / (c - b))), 0);
 }
+
 
 
 
@@ -77,49 +78,62 @@ void setup() {
 
 
 void loop() {
- suhu_z = dht.readTemperature();
-  roundsuhu = round(suhu_z); // Melakukan operasi pembulatan nilai suhu yang terbaca
-  lcd.setCursor(13, 0);
-  lcd.print(roundsuhu);
+   delay(2000); // Wait 2 seconds before next loop
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  roundkelembaban = round(h);
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  roundsuhu = round(t);
+
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float kelembabanValue = roundkelembaban;
+  // Read temperature as Celsius (the default)
+  float suhuValue = roundsuhu;
   
-  kelembaban_z = dht.readHumidity();
-  roundkelembaban = round(kelembaban_z); // Melakukan operasi pembulatan nilai kelembaban yang terbaca
-  lcd.setCursor(13, 1);
-  lcd.print(roundkelembaban);
-  
+
   /* Bagian 3 : Input Crips */
   suhu Suhu;
   kelembaban Kelembaban;
-  
-  /* Bagian 4 : Fungsi Keanggotaan Himpunan Fuzzy dan Domainnya*/
-  / Membuat fungsi keanggotaan input suhu /
-  Suhu.sangat_dingin = tri_mf(0, 0, 10, roundsuhu);
-  Suhu.dingin = tri_mf(5, 10, 20, roundsuhu);
-  Suhu.normal = tri_mf(15, 20, 30, roundsuhu);
-  Suhu.panas = tri_mf(25, 30, 37, roundsuhu);
-  Suhu.sangat_panas = tri_mf(30, 40, 40, roundsuhu);
-  / Membuat fungsi keanggotaan input kelembaban /
-  Kelembaban.sangat_kering = trap_mf(0, 0, 10, 20, roundkelembaban);
-  Kelembaban.kering = trap_mf(10, 20, 30, 40, roundkelembaban);
-  Kelembaban.sedang = trap_mf(30, 40, 60, 70, roundkelembaban);
-  Kelembaban.basah = trap_mf(60, 70, 80, 90, roundkelembaban);
-  Kelembaban.sangat_basah = trap_mf(80, 90, 100, 100, roundkelembaban);
-  
-  / Membuat fungsi keanggotaan output durasi penyiraman /
-  float sangat_singkat = 0 ;
-  float singkat = 6 ;
-  float menengah = 12 ;
-  float lama = 18 ;
-  float sangat_lama = 25 ;
-  
+
+    /* Bagian 4 : Fungsi Keanggotaan Himpunan Fuzzy dan Domainnya*/
+  Suhu.sangat_dingin = trap_mf(0, 0, 5, 10, suhuValue);
+  Suhu.dingin = trap_mf(5, 10, 20, 25, suhuValue);
+  Suhu.normal = trap_mf(20, 25, 30, 35, suhuValue);
+  Suhu.panas = trap_mf(30, 35, 40, 45, suhuValue);
+  Suhu.sangat_panas = trap_mf(40, 45, 50, 50, suhuValue);
+
+  Kelembaban.sangat_kering = trap_mf(0, 0, 10, 20, kelembabanValue);
+  Kelembaban.kering = trap_mf(10, 20, 30, 40, kelembabanValue);
+  Kelembaban.sedang = trap_mf(30, 40, 60, 70, kelembabanValue);
+  Kelembaban.basah = trap_mf(60, 70, 80, 90, kelembabanValue);
+  Kelembaban.sangat_basah = trap_mf(80, 90, 100, 100, kelembabanValue);
+
+//Membuat fungsi keanggotaan output pwm kecepatan
+// Fuzzy output sets for Fan Speed
+float sangatPelan =0;
+float pelan =25;
+float sedangKecepatan =50;
+float cepat =75;
+float sangatCepat =100; 
+
+
   float bobot1 = 0, bobot2 = 0, bobot3 = 0, bobot4 = 0, bobot5 = 0;
   float bobot6 = 0, bobot7 = 0, bobot8 = 0, bobot9 = 0, bobot10 = 0;
   float bobot11 = 0, bobot12 = 0, bobot13 = 0, bobot14 = 0, bobot15 = 0;
   float bobot16 = 0, bobot17 = 0, bobot18 = 0, bobot19 = 0, bobot20 = 0;
   float bobot21 = 0, bobot22 = 0, bobot23 = 0, bobot24 = 0, bobot25 = 0;
-  /* Bagian 5 : Membuat Fuzzy Rule Base dan Proses Implikasi (MIN) */
-  / DP = durasi penyiraman /
-         // (1) Jika Suhu Sangat Dingin dan Kelembaban Sangat Kering maka DP Lama
+
+/* Bagian 5 : Membuat Fuzzy Rule Base dan Proses Implikasi (MIN) */
+ // (1) Jika Suhu Sangat Dingin dan Kelembaban Sangat Kering maka DP Lama
   if (Suhu.sangat_dingin > 0 && Kelembaban.sangat_kering > 0 ) {
     bobot1 = min(Suhu.sangat_dingin, Kelembaban.sangat_kering);
   }
@@ -234,50 +248,48 @@ void loop() {
                       bobot7 + bobot8 + bobot9 + bobot10 + bobot11 + bobot12 + bobot13 + bobot14 +
                       bobot15 + bobot16 + bobot17 + bobot18 + bobot19 + bobot20 + bobot21 + bobot22
                       + bobot23 + bobot24 + bobot25;
-  durasi_z = weightedsum / sumofweight;
+  pwm_z = weightedsum / sumofweight;
 
-  
-  /* Bagian 7 : Display Serial Monitor*/
-  Serial.print("Suhu : ");
-  Serial.print(roundsuhu);
-  Serial.print(" *C, ");
-  Serial.print("Kelembaban : ");
-  Serial.print(roundkelembaban);
+ /* Bagian 7 : Display Serial Monitor*/
+  Serial.println("\n\n\nInputs:");
+  Serial.print("Kelembaban: ");
+  Serial.println(kelembabanValue);
   Serial.print(" %, ");
-  Serial.print("Durasi: ");
-  Serial.print(durasi_z);
-  Serial.print(" s");
+  Serial.print("Suhu: ");
+  Serial.println(suhuValue);
+  Serial.print(" *C, ");
+  Serial.print("PWM: ");
+  Serial.println(pwm_z);
+  Serial.print(" m/s");
   Serial.println(" ");
   
-  /* Bagian 8 : Display LCD I2C 20x4 */
-  if (durasi_z > 0) {
-    digitalWrite(pindurasi, HIGH);
-    lcd.setCursor(13, 2);
-    lcd.print(durasi_z);
-    lcd.setCursor(13, 3);
-    lcd.print("Aktif");
-    delay(durasi_z * 1000);
-    digitalWrite(pindurasi, LOW);
-    lcd.setCursor(13, 3);
-    lcd.print(" ");
-  }
-  lcd.setCursor(13, 3);
-  lcd.print("Mati");
-  delay(2000);
+  
+  // Konversi pwm_z ke nilai PWM 0-255
+  int pwmValue = map(pwm_z, 0, 100, 0, 255);
+  Serial.print("Nilai PWM: ");
+  Serial.println(pwmValue);
+  
+  // Mengatur kecepatan kipas menggunakan PWM pada pin D8
+  analogWrite(D8, pwmValue);
   
   // Blynk.run(); // Jalankan Blynk
   
   // // Kirim data ke Blynk
-  // Blynk.virtualWrite(V0, roundkelembaban); // Kirim kelembaban ke virtual pin V0
-  // Blynk.virtualWrite(V1, roundsuhu); // Kirim suhu ke virtual pin V1
-  // Blynk.virtualWrite(V2, durasi_z); // Kirim kecepatan kipas ke virtual pin V2
+  // Blynk.virtualWrite(V0, kelembabanValue); // Kirim kelembaban ke virtual pin V0
+  // Blynk.virtualWrite(V1, suhuValue); // Kirim suhu ke virtual pin V1
+  // Blynk.virtualWrite(V2, kecepatanValue); // Kirim kecepatan kipas ke virtual pin V2
 
   // Tampilkan kelembaban dan suhu di LCD satu baris
   lcd.setCursor(0, 0);
   lcd.print("Rh:");
-  lcd.print(roundkelembaban);
+  lcd.print(kelembabanValue);
   lcd.print("T:");
-  lcd.print(roundsuhu);
+  lcd.print(suhuValue);
 
-
+  //lcd kecepatan kipas dan nilai pwm
+  lcd.setCursor(0, 1); 
+  lcd.print("Kec:");
+  lcd.print(kecepatanValue);
+  lcd.print("PWM:");
+  lcd.print(pwmValue);
 }
